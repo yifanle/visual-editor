2ad1e4931ca9ff7e36e00ad3336777287e19b590
yifanle <2878725498@qq.com>
2023-04-11 07:14:08 +0800

    添加adminLayout左侧菜单

---
+++ src/components/admin/menu/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-menu default-active="2" class="el-menu-vertical" :collapse="porps.isCollapse">
+    <el-sub-menu index="1">
+      <template #title>
+        <svg-icon name="ele-Location" :size="16" />
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item-group>
+        <template #title><span>Group One</span></template>
+        <el-menu-item index="1-1">item one</el-menu-item>
+        <el-menu-item index="1-2">item two</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="1-3">item three</el-menu-item>
+      </el-menu-item-group>
+      <el-sub-menu index="1-4">
+        <template #title><span>item four</span></template>
+        <el-menu-item index="1-4-1">item one</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <svg-icon name="ele-Menu" :size="16" />
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <svg-icon name="ele-Document" :size="16" />
+      <template #title>Navigator Three</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <svg-icon name="ele-Setting" :size="16" />
+      <template #title>Navigator Four</template>
+    </el-menu-item>
+  </el-menu>
+</template>
+<script setup lang="ts" name="WeMenu">
+const porps = defineProps({
+  isCollapse: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+</script>
+<style scoped lang="scss">
+.el-menu-vertical {
+  width: 100%;
+  height: 100%;
+  border-right: none;
+}
+</style>
--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,9 +1,64 @@
 <template>
-    <div>
-        admin
-    </div>
+    <el-container>
+        <el-aside class="left-materials" :class="isCollapse ? 'hide' : 'show'">
+            <div class="collapse-btn" @click="() => isCollapse = !isCollapse">
+                <svg-icon v-if="!isCollapse" name="tab-CaretLeft" :size="18" />
+                <svg-icon v-else name="tab-CaretRight" :size="18" />
+            </div>
+            <we-menu :isCollapse="isCollapse"></we-menu>
+        </el-aside>
+        <el-main>
+            <router-view></router-view>
+        </el-main>
+    </el-container>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
+import WeMenu from '@/components/admin/menu/index.vue'
+// 用于控制左侧面板的折叠
+const isCollapse = ref(false)
+
+
 </script>
 <style scoped lang="scss">
-</style>
+$collapseBtnWidth: 20px;
+.left-materials {
+    overflow: unset;
+    position: relative;
+    border-right: #efefef 1px solid;
+    padding-top: 5px;
+
+    .collapse-btn {
+        $height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        width: $collapseBtnWidth;
+        height: $height;
+        border-radius: 5px;
+        z-index: 2;
+        background-color: #fff;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
+        right: calc($collapseBtnWidth / -2);
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+        transition: all 0.5s;
+
+        &:hover {
+            background-color: #ececec;
+        }
+    }
+    
+}
+
+.hide {
+    width: calc(var(--el-menu-icon-width) + var(--el-menu-base-level-padding) * 2);
+    transition: width 0.5s;
+}
+
+.show {
+    width: 300px;
+    transition: width 0.5s;
+}</style>
