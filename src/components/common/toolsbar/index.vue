2de6c4c69d3b6cbd1ae09ae581252626be46b5e7
yifanle <2878725498@qq.com>
2023-05-09 19:05:44 +0800

    调试monaco-editor

--- src/components/common/toolsbar/index.vue
+++ src/components/common/toolsbar/index.vue
@@ -1,37 +1,73 @@
 <template>
-    <div class="tools" v-if="showToolbar">
-        <el-tooltip :content="button.shortcut == '' ?`${button.label}` : `${button.label}(${button.shortcut})`" placement="bottom" effect="light" v-for="button in buttons">
-            <div class="tool-btn" @click="button.handler()">
-                <svg-icon :size="20" :name="button.icon" />
-            </div>
-        </el-tooltip>
-    </div>
+  <div class="tools" v-if="showToolbar">
+    <el-tooltip :content="button.shortcut == '' ? `${button.label}` : `${button.label}(${button.shortcut})`"
+      placement="bottom" effect="light" v-for="button in buttons">
+      <div class="tool-btn" @click="button.handler()">
+        <svg-icon :size="20" :name="button.icon" />
+      </div>
+    </el-tooltip>
+  </div>
+  <el-dialog v-model="dialogVisible" title="Tips" width="30%" draggable :modal="false" :close-on-click-modal="false">
+    <monaco-edit
+      ref="monacoEditRef"
+      code="{console.log('123');console.log('456');}"
+      :language="IMonacoLanguage.TypeScript"
+      :readonly="readOnly"
+    ></monaco-edit>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="()=> editToggle()">{{btnText}}</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 <script setup lang="ts" name="ToolsBar">
-import { ref ,watch} from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import UseCommands from '@/utils/commands/UseCommands';
+import monacoEdit from '@/components/monaco-edit/index.vue';
+import { IMonacoLanguage } from '@/components/monaco-edit/model/model';
+
+const dialogVisible = ref(false);
+const readOnly = ref(true);
+const btnText = ref('编辑');
+const monacoEditRef = ref();
+const editToggle = () => {
+  readOnly.value = !readOnly.value;
+  if(readOnly.value){
+    btnText.value = '编辑';
+  }else{
+    btnText.value = '保存';
+  }
+}
+const openDialog = () => {
+  dialogVisible.value = true;
+}
+const closeDialog = () => {
+  dialogVisible.value = false;
+}
+
 const route = useRoute();
 const showToolbar = ref(false);
 const useCommands = UseCommands.getInstance();
 useCommands.assembl();
 const command = {
-    redo: () => useCommands.commands.redo(),
-    undo: () => useCommands.commands.undo(),
-    other: () => alert('正在努力开发中...')
+  redo: () => useCommands.commands.redo(),
+  undo: () => useCommands.commands.undo(),
+  other: () => alert('正在努力开发中...')
 }
 const buttons = [
-    {label:'保存',icon:'tab-FileCheck',shortcut:'ctrl+S',handler:command.other},
-    {label:'复制',icon:'tab-Files',shortcut:'ctrl+C',handler:command.other},
-    {label:'撤销',icon:'tab-ArrowBackUp',shortcut:'ctrl+Z',handler:command.undo},
-    {label:'恢复',icon:'tab-ArrowForwardUp',shortcut:'ctrl+Y', handler:command.redo},
-    {label:'清空',icon:'tab-TrashX',shortcut:'ctrl+Delete', handler:command.other},
-    {label:'导入json数据',icon:'tab-FileCode',shortcut:'', handler:command.other},
-    {label:'导出json数据',icon:'tab-FileDownload',shortcut:'', handler:command.other},
-    {label:'预览',icon:'tab-Devices',shortcut:'', handler:command.other},
+  { label: '保存', icon: 'tab-FileCheck', shortcut: 'ctrl+S', handler: command.other },
+  { label: '复制', icon: 'tab-Files', shortcut: 'ctrl+C', handler: command.other },
+  { label: '撤销', icon: 'tab-ArrowBackUp', shortcut: 'ctrl+Z', handler: command.undo },
+  { label: '恢复', icon: 'tab-ArrowForwardUp', shortcut: 'ctrl+Y', handler: command.redo },
+  { label: '清空', icon: 'tab-TrashX', shortcut: 'ctrl+Delete', handler: command.other },
+  { label: '导入json数据', icon: 'tab-FileCode', shortcut: '', handler: command.other },
+  { label: '导出json数据', icon: 'tab-FileDownload', shortcut: '', handler: ()=>openDialog() },
+  { label: '预览', icon: 'tab-Devices', shortcut: '', handler: command.other },
 ]
 
-watch(route,()=>{
+watch(route, () => {
   toggleToolbar();
 })
 
@@ -46,20 +82,21 @@ const toggleToolbar = () => {
 </script>
 <style scoped lang="scss">
 .tools {
-    height: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .tool-btn {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
-    .tool-btn {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      padding: 8px 8px;
-      margin-right: 30px;
-      color: #4169e1;
-      user-select: none;
-      cursor: pointer;
-    }
+    padding: 8px 8px;
+    margin-right: 30px;
+    color: #4169e1;
+    user-select: none;
+    cursor: pointer;
   }
+}
 </style>
--- src/components/monaco-edit/index.vue
+++ src/components/monaco-edit/index.vue
@@ -2,7 +2,7 @@
     <div class="monaco" ref="monacoRef"></div>
 </template>
 <script setup lang="ts" name="MonacoEdit">
-import { PropType, ref, nextTick } from 'vue';
+import { PropType, ref, nextTick, computed ,watch} from 'vue';
 import monaco from './customMonaco';
 import { IMonacoLanguage } from './model/model';
 const props = defineProps({
@@ -22,22 +22,62 @@ const props = defineProps({
 const emit = defineEmits(['onDidChangeModelContent']);
 const monacoRef = ref<HTMLElement | null>(null);
 let editor: monaco.editor.IStandaloneCodeEditor;
+const readonly = computed(() => props.readonly);
+watch(readonly, (newVal, oldVal) => {
+    if(editor) {
+        editor.updateOptions({
+            readOnly: newVal
+        })
+    }
+})
 nextTick(() => {
     if (monacoRef.value) {
         editor = monaco.editor.create(monacoRef.value, {
-            value: props.code,
             language: props.language,
             automaticLayout: true,
             roundedSelection: true,
             scrollBeyondLastLine: true,
             readOnly: props.readonly,
-            theme: 'vs-dark'
+            theme: 'vs'
         });
+        if(props.readonly) {
+            editor.setValue(checkJsonCode(props.code));
+        }else {
+            editor.setValue(props.code);
+            editor.getAction('editor.action.formatDocument')?.run();//自动格式化代码
+            editor.setValue(editor.getValue());//再次设置
+            // value = props.code
+        }
         editor.onDidChangeModelContent((e) => {
             emit('onDidChangeModelContent', editor.getValue());
         })
     }
 });
+const checkJsonCode = (strJsonCode:string) => {
+  let res = '';
+  try {
+    for (let i = 0, j = 0, k = 0, ii, ele; i < strJsonCode.length; i++) {
+      ele = strJsonCode.charAt(i);
+      if (j % 2 === 0 && ele === '}') {
+        k--;
+        for (ii = 0; ii < k; ii++) ele = `    ${ele}`;
+        ele = `\n${ele}`;
+      } else if (j % 2 === 0 && ele === '{') {
+        ele += '\n';
+        k++;
+        for (ii = 0; ii < k; ii++) ele += '    ';
+      } else if (j % 2 === 0 && ele === ',') {
+        ele += '\n';
+        for (ii = 0; ii < k; ii++) ele += '    ';
+      } else if (ele === '"') j++;
+      res += ele;
+    }
+  } catch (error) {
+    res = strJsonCode;
+  }
+  return res;
+}
+
 const disposeEdit = () => {
     editor.dispose();
 }
@@ -46,4 +86,7 @@ defineExpose({
 })
 </script>
 <style scoped lang="scss">
+.monaco {
+    height: 400px;
+}
 </style>
