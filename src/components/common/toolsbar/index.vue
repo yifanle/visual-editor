061d7383d8cf76f8bb5f61f3a91bce7c44b718e4
yifanle <2878725498@qq.com>
2023-05-10 19:15:42 +0800

    修改了一个小bug

--- src/components/common/toolsbar/index.vue
+++ src/components/common/toolsbar/index.vue
@@ -7,17 +7,13 @@
       </div>
     </el-tooltip>
   </div>
-  <el-dialog v-model="dialogVisible" title="编辑JsonSchema" width="30%" draggable :modal="false" :before-close="closeDialog" :close-on-click-modal="false">
-    <monaco-edit
-      ref="monacoEditRef"
-      :code="jsonCode"
-      :language="IMonacoLanguage.JSON"
-      :readonly="readOnly"
-      @onDidChangeModelContent="(codereturn) => onDidChangeModelContent(codereturn)"
-    ></monaco-edit>
+  <el-dialog v-model="dialogVisible" title="编辑JsonSchema" width="30%" draggable :modal="false" :before-close="closeDialog"
+    :close-on-click-modal="false">
+    <monaco-edit ref="monacoEditRef" :code="jsonCode" :language="IMonacoLanguage.JSON" :readonly="readOnly"
+      @onDidChangeModelContent="(codereturn) => onDidChangeModelContent(codereturn)"></monaco-edit>
     <template #footer>
       <span class="dialog-footer">
-        <el-button type="primary" @click="()=> editToggle()">{{btnText}}</el-button>
+        <el-button type="primary" @click="() => editToggle()">{{ btnText }}</el-button>
       </span>
     </template>
   </el-dialog>
@@ -29,43 +25,70 @@ import UseCommands from '@/utils/commands/UseCommands';
 import monacoEdit from '@/components/monaco-edit/index.vue';
 import { IMonacoLanguage } from '@/components/monaco-edit/model/model';
 import EmitterUtil from '@/utils/EmitterUtil';
+import { ElMessage, ElMessageBox, messageType } from 'element-plus'
+
+const openBox = (content: string, title: string, type: string = 'warning', success: () => any, fail: () => any) => {
+  ElMessageBox.confirm(
+    content,
+    title,
+    {
+      confirmButtonText: '确认',
+      cancelButtonText: '取消',
+      type: type as messageType,
+    }
+  )
+    .then(success)
+    .catch(fail)
+}
 
 const dialogVisible = ref(false);
 const readOnly = ref(true);
+const save = ref(false);
 const btnText = ref('编辑');
+// todo: unmounted时调用 monacoEditRef.value.dispose();
 const monacoEditRef = ref();
 let dataRef = ref();
 let jsonCode = ref();
-let before:any = null;
+let before: any = null;
 
-EmitterUtil.register('setData',(data:any)=> {
+// 用于接受jsonSchema数据
+EmitterUtil.register('setData', (data: any) => {
   dataRef = data;
   jsonCode.value = JSON.stringify(dataRef.value);
   useCommands.setData(dataRef);
 })
 
-
-const onDidChangeModelContent = (codereturn:any)=> {
+// monaco编辑器内容改变回调
+const onDidChangeModelContent = (codereturn: any) => {
   jsonCode.value = codereturn;
   dataRef.value = JSON.parse(jsonCode.value);
 }
 
+// 切换编辑状态
 const editToggle = () => {
   readOnly.value = !readOnly.value;
-  if(readOnly.value){
+  // 用于标记是否保存
+  save.value = false;
+  if (readOnly.value) {
     btnText.value = '编辑';
+    save.value = true;
+    dataRef.value = before;
+    // 保存更新画布，并将操作防止命令栈中
     useCommands.commands.updateCanvas(JSON.parse(jsonCode.value))
-  }else{
+  } else {
     btnText.value = '保存';
   }
 }
 const openDialog = () => {
   dialogVisible.value = true;
   jsonCode.value = JSON.stringify(dataRef.value);
+  // 记录当前数据，用于未保存时恢复数据
   before = dataRef.value;
 }
 const closeDialog = () => {
-  dataRef.value = before;
+  // 如果没保存，在关闭对话框时恢复数据
+  if (!save.value)
+    dataRef.value = before;
   dialogVisible.value = false;
   readOnly.value = true;
   btnText.value = '编辑';
@@ -85,16 +108,29 @@ const buttons = [
   { label: '复制', icon: 'tab-Files', shortcut: 'ctrl+C', handler: command.other },
   { label: '撤销', icon: 'tab-ArrowBackUp', shortcut: 'ctrl+Z', handler: command.undo },
   { label: '恢复', icon: 'tab-ArrowForwardUp', shortcut: 'ctrl+Y', handler: command.redo },
-  { label: '清空', icon: 'tab-TrashX', shortcut: 'ctrl+Delete', handler: command.other },
-  { label: '查看json数据', icon: 'tab-FileCode', shortcut: '', handler: ()=>openDialog() },
+  {
+    label: '清空', icon: 'tab-TrashX', shortcut: 'ctrl+Delete', handler: () => openBox('是否确认清空画布？', '确认', 'warning', () => {
+      ElMessage({
+        type: 'success',
+        message: '清空成功',
+        duration: 1500
+      })
+    }, () => {
+      ElMessage({
+        type: 'success',
+        message: '已取消！',
+        duration: 1500
+      })
+    })
+  },
+  { label: '查看json数据', icon: 'tab-FileCode', shortcut: '', handler: () => openDialog() },
   { label: '预览', icon: 'tab-Devices', shortcut: '', handler: command.other },
 ]
 
 watch(route, () => {
+  // 当路由变化时，判断是否显示工具栏
   toggleToolbar();
 })
-
-
 
 const toggleToolbar = () => {
   const path = route.path.split('?')[0];
